178342b915fcfdea80750de53e5c98b5d90947c6
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-01-20 11:11:04 -0300

    feat: Refactoring routes

--- backend/Routes/tasksRoutes.js
+++ backend/Routes/tasksRoutes.js
@@ -3,13 +3,14 @@ const { Router } = require('express');
 
 const taskController = new TaskController();
 
-const router = Router()
+const router = Router();
 
-router.get('/tasks/all', (req, res) => taskController.getAllRegisters(req, res));
-router.post('/task/create', (req, res) => taskController.create(req, res));
-router.put('/task/:id/updating', (req, res) => taskController.update(req, res));
-router.delete('/task/:id/delete', (req, res) => taskController.deleted(req, res));
+router.get('/tasks', (req, res) => taskController.getAllRegisters(req, res));
+router.get('/tasks/:id', (req, res) => taskController.getOneRegister(req, res));
+router.post('/tasks', (req, res) => taskController.create(req, res));
+router.put('/tasks/:id', (req, res) => taskController.update(req, res));
+router.delete('/tasks/:id', (req, res) => taskController.deleted(req, res));
 
 
-module.exports = router
+module.exports = router;
 
--- backend/Routes/usersRoutes.js
+++ backend/Routes/usersRoutes.js
@@ -7,8 +7,8 @@ const router = Router();
 router.get('/users', (req, res) => userController.getAllRegisters(req, res));
 router.get('/users/:id', (req, res) => userController.getOneRegister(req, res));
 router.get('/users/:user_id/tasks', (req, res) => userController.takeAllTasks(req, res));
-router.post('/users/Create-an-account', (req, res) => userController.create(req, res));
-router.put('/users/:id/updatingData', (req, res) => userController.update(req, res));
-router.delete('/users/:id/delete', (req, res) => userController.deleted(req, res));
+router.post('/users', (req, res) => userController.create(req, res));
+router.put('/users/:id', (req, res) => userController.update(req, res));
+router.delete('/users/:id', (req, res) => userController.deleted(req, res));
 
 module.exports = router;
